49f3b2964cfe7dfd2fd65d874a9c33c5656b72f4
Hazhaz1412 <hoanghuanpham3@gmail.com>
2026-04-07 20:46:55 +0700

    feat: lots stuffs

--- Backend/middlewares/upload.middleware.js
+++ Backend/middlewares/upload.middleware.js
@@ -2,25 +2,8 @@ const multer = require('multer');
 
 const storage = multer.memoryStorage();
 
-const allowedMimeTypes = [
-    'application/pdf', // File PDF
-    'image/jpeg',      // Ảnh JPG/JPEG
-    'image/png',       // Ảnh PNG
-    'application/vnd.openxmlformats-officedocument.wordprocessingml.document', // File Word (.docx)
-    'application/vnd.ms-excel' // File Excel (.xls)
-];
-
-const fileFilter = (req, file, cb) => {
-    if (allowedMimeTypes.includes(file.mimetype)) {
-        cb(null, true); 
-    } else {
-        cb(new Error('Only PDF, JPG, PNG, DOCX, and XLS files are allowed!'), false);
-    }
-};
-
 const upload = multer({ 
     storage: storage,
-    fileFilter: fileFilter,
     limits: { fileSize: 10 * 1024 * 1024 } //10MB
 });
 
